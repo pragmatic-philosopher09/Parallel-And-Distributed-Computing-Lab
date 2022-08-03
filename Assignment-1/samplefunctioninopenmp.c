d81373c58fc16f0d3dc09e6bfb92516574abfb0a
Priyanshu Mohanty <prince91001@gmail.com>
2022-08-03 15:43:08 +0530

    Added A File

---
+++ Assignment-1/samplefunctioninopenmp.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+int main(int argc, char *argv[])
+{
+    int nthreads, tid, procs, maxt, inpar, dynamic, nested;
+    /* Start parallel region */
+#pragma omp parallel num_threads(100) private(nthreads, tid)
+    {
+        /* Obtain thread number */
+        tid = omp_get_thread_num(); /* Only master thread does this */
+                                    /* Get environment information */
+        procs = omp_get_num_procs();
+        nthreads = omp_get_num_threads();
+        maxt = omp_get_max_threads();
+        inpar = omp_in_parallel();
+        dynamic = omp_get_dynamic();
+        nested = omp_get_nested();
+        if (tid != 0)
+        {
+            printf("Thread %d getting environment info...\n", tid);
+
+            /* Print environment information */
+            printf("Number of processors = %d\n", procs);
+            printf("Number of threads = %d\n", nthreads);
+            printf("Max threads = %d\n", maxt);
+            printf("In parallel? = %d\n", inpar);
+            printf("Dynamic threads enabled? = %d\n", dynamic);
+            printf("Nested parallelism supported? = %d\n", nested);
+        }
+    }
+
+    return 0;
+}
